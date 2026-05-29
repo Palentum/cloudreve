bb03bd3968ca36ea509b3928b2d40c1eebba0f14
Palentum <79781425+Palentum@users.noreply.github.com>
2026-05-29 13:13:31 +0800

    fix(security): enable captcha by default for login and registration

--- models/defaults.go
+++ models/defaults.go
@@ -45,8 +45,8 @@ var defaultSettings = []Setting{
 	{Name: "onedrive_source_timeout", Value: `1800`, Type: "timeout"},
 	{Name: "reset_after_upload_failed", Value: `0`, Type: "upload"},
 	{Name: "use_temp_chunk_buffer", Value: `1`, Type: "upload"},
-	{Name: "login_captcha", Value: `0`, Type: "login"},
-	{Name: "reg_captcha", Value: `0`, Type: "login"},
+	{Name: "login_captcha", Value: `1`, Type: "login"},
+	{Name: "reg_captcha", Value: `1`, Type: "login"},
 	{Name: "email_active", Value: `0`, Type: "register"},
 	{Name: "mail_activation_template", Value: `<!DOCTYPE html PUBLIC"-//W3C//DTD XHTML 1.0 Transitional//EN""http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; box-sizing: border-box;
 font-size: 14px; margin: 0;"><head><meta name="viewport"content="width=device-width"/><meta http-equiv="Content-Type"content="text/html; charset=UTF-8"/><title>激活您的账户</title><style type="text/css">img{max-width:100%}body{-webkit-font-smoothing:antialiased;-webkit-text-size-adjust:none;width:100%!important;height:100%;line-height:1.6em}body{background-color:#f6f6f6}@media only screen and(max-width:640px){body{padding:0!important}h1{font-weight:800!important;margin:20px 0 5px!important}h2{font-weight:800!important;margin:20px 0 5px!important}h3{font-weight:800!important;margin:20px 0 5px!important}h4{font-weight:800!important;margin:20px 0 5px!important}h1{font-size:22px!important}h2{font-size:18px!important}h3{font-size:16px!important}.container{padding:0!important;width:100%!important}.content{padding:0!important}.content-wrap{padding:10px!important}.invoice{width:100%!important}}</style></head><body itemscope itemtype="http://schema.org/EmailMessage"style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing:
